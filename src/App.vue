e02b70405a4e5ec2247a78dc7efa12a3e731ec8f
gelatoYu <164867859@qq.com>
2019-01-13 11:40:21 +0800

    manager---完成登录页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
-  <div id="app">
+  <div class="login-container">
+    <div class="form-container">
+      <el-form label-position="top" label-width="80px" :model="formData">
+        <h2>用户登录</h2>
+        <el-form-item label="姓 名:">
+          <el-input v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密 码:">
+          <el-input v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-button class="btn-login" type="primary">主要按钮</el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
-}
+  name: "app",
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.login-container {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-container {
+    width: 580px;
+    background: #fff;
+    padding: 40px;
+    border-radius: 10px;
+    .btn-login {
+      width: 100%;
+    }
+  }
 }
 </style>
